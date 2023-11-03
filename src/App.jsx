376c2156110abe4c155f3d1630904c15e5b18692
LazyDotExe <papaporo@proton.me>
2023-11-03 17:27:48 -0500

    added another api call via axios.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 const XIVAPI = require('@xivapi/js')
 const xiv = new XIVAPI({
   private_key: '3419471f8b0b44a4b43419980053b67ee01339430bed425a86260bf096f284e4',
@@ -8,9 +9,13 @@ const xiv = new XIVAPI({
 })
 
 
+
+
 function App() {
 
   const [imageUrl, setImageUrl] = useState([]);
+  const [imagePoro, setImagePoro] = useState([]);
+
   let myItem;
   let myFc;
 
@@ -47,11 +52,14 @@ function App() {
   async function fetchData() {
     try {
       const searchResponse = await xiv.freecompany.search('Pixel Pirate Academy', { server: 'Zalera' });
+      
       const myFc = searchResponse.results[0].id;
+      console.log("Here is the ID'S", searchResponse)
 
 
       const getResponse = await xiv.freecompany.get(myFc, { data: 'FCM' });
       // console.log(getResponse.free_company_members);
+      console.log("List of all FC members", getResponse.free_company_members)
       setImageUrl(getResponse.free_company_members);
 
     } catch (error) {
@@ -63,6 +71,17 @@ function App() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  axios.get('https://xivapi.com/character/20542064')
+  .then((response) => {
+    // Handle the API response data here
+    console.log('API Response:', response.data);
+    setImagePoro(response.data.Character.Portrait)
+  })
+  .catch((error) => {
+    // Handle any errors that occur during the request
+    console.error('Error:', error);
+  });
 
 
   return (
@@ -76,12 +95,14 @@ function App() {
 
         )}
         {imageUrl.length > 0 ? (
-          <img src={imageUrl[0].avatar}></img>
+          // <img className='card' src={imageUrl[0].avatar}></img>
+          <img className='card' src={imagePoro}></img>
+
         ) : (
           <p>{null}</p>
 
         )}
-      </header> 
+      </header>
     </div>
   );
 }
